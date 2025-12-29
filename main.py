bae0231323c38b3db1494250a0587f88cbe2ce64
GhostGamer6969 <arambhmani@hotmail.com>
2025-12-29 21:06:06 +0530

    vector store

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from langchain_ollama import ChatOllama
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
+from langchain_core.vectorstores import InMemoryVectorStore
 
 ## pdf loading
 file_path = "./nke-10k-2023.pdf"
@@ -20,6 +21,12 @@ vector_1 = embeddings.embed_query(all_splits[0].page_content)
 vector_2 = embeddings.embed_query(all_splits[1].page_content)
 
 assert len(vector_1) == len(vector_2)
-print(f"Generated vectors of length {len(vector_1)}\n")
-print(vector_1[:10])
 
+## Vectore stores
+vector_store = InMemoryVectorStore(embeddings)
+ids = vector_store.add_documents(documents=all_splits)
+results = vector_store.similarity_search(
+    "How many distribution centers does Nike have in the US?"
+)
+
+print(results)
