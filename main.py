a2881c2805fbf13b0a0cf098a49668a5427525ae
GhostGamer6969 <arambhmani@hotmail.com>
2025-12-29 14:21:15 +0530

    Embeddings

--- main.py
+++ main.py
@@ -3,28 +3,23 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
 
+## pdf loading
 file_path = "./nke-10k-2023.pdf"
 loader = PyPDFLoader(file_path)
 docs = loader.load()
+
+## splitting
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000, chunk_overlap=200, add_start_index=True
 )
 all_splits = text_splitter.split_documents(docs)
 
-print(len(all_splits))
-# llm = ChatOllama(
-#         model="deepseek-r1:1.5b",
-#         temperature=0,
-#         base_url="http://localhost:11434"
-#         )
-#
-# prompt = "who is the document from"
-# print(docs[0].page_content[:200])
-# msgs=[
-#         {"role":"system","content":"You are an ai assistant that explains the docs"},
-#         {"role":"system","content":docs[0].page_content[:200]},
-#         {"role":"user","content":prompt}
-#         ]
-# resp=llm.invoke(msgs).content
-# print(resp)
-#
+## embedding
+embeddings = OllamaEmbeddings(model="deepseek-r1:1.5b")
+vector_1 = embeddings.embed_query(all_splits[0].page_content)
+vector_2 = embeddings.embed_query(all_splits[1].page_content)
+
+assert len(vector_1) == len(vector_2)
+print(f"Generated vectors of length {len(vector_1)}\n")
+print(vector_1[:10])
+
