fa03762fb56d9a1bbdfa3fccd5176bd2ef0d827b
GhostGamer6969 <arambhmani@hotmail.com>
2025-12-29 14:17:25 +0530

    documents and document loader

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 from langchain_ollama import ChatOllama
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
 
-llm = ChatOllama(
-        model="deepseek-r1:1.5b",
-        temperature=0,
-        base_url="http://localhost:11434"
-        )
+file_path = "./nke-10k-2023.pdf"
+loader = PyPDFLoader(file_path)
+docs = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=200, add_start_index=True
+)
+all_splits = text_splitter.split_documents(docs)
 
-prompt = "Hi! What's up?"
-msgs=[
-        {"role":"system","content":"You are an ai assistant"},
-        {"role":"user","content":prompt}
-        ]
-resp=llm.invoke(msgs).content
-print(resp)
+print(len(all_splits))
+# llm = ChatOllama(
+#         model="deepseek-r1:1.5b",
+#         temperature=0,
+#         base_url="http://localhost:11434"
+#         )
+#
+# prompt = "who is the document from"
+# print(docs[0].page_content[:200])
+# msgs=[
+#         {"role":"system","content":"You are an ai assistant that explains the docs"},
+#         {"role":"system","content":docs[0].page_content[:200]},
+#         {"role":"user","content":prompt}
+#         ]
+# resp=llm.invoke(msgs).content
+# print(resp)
+#
