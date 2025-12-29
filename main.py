b3d3068cdaaa17a8db8bb29cde275fbaa29402f4
GhostGamer6969 <arambhmani@hotmail.com>
2025-12-29 21:42:53 +0530

    vector stores pt 2

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ text_splitter = RecursiveCharacterTextSplitter(
 all_splits = text_splitter.split_documents(docs)
 
 ## embedding
+# embeddings = OllamaEmbeddings(model="deepseek-r1:8b")
 embeddings = OllamaEmbeddings(model="deepseek-r1:1.5b")
 vector_1 = embeddings.embed_query(all_splits[0].page_content)
 vector_2 = embeddings.embed_query(all_splits[1].page_content)
@@ -25,8 +26,16 @@ assert len(vector_1) == len(vector_2)
 ## Vectore stores
 vector_store = InMemoryVectorStore(embeddings)
 ids = vector_store.add_documents(documents=all_splits)
-results = vector_store.similarity_search(
-    "How many distribution centers does Nike have in the US?"
-)
+# results = vector_store.similarity_search(
+#     "How many distribution centers does Nike have in the US?"
+# )
+#
+# print(results[0])
+embedding = embeddings.embed_query("How were Nike's margins impacted in 2023?")
 
-print(results)
+results = vector_store.similarity_search_by_vector(embedding)
+print(results[0])
+# results = vector_store.similarity_search_with_score("What was Nike's revenue in 2023?")
+# doc, score = results[0]
+# print(f"Score: {score}\n")
+# print(doc)
