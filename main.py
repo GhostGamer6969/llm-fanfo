d45337f7d5a0e24c6b1bf40ad21b32bc8d01ef08
GhostGamer6969 <arambhmani@hotmail.com>
2025-12-30 13:07:51 +0530

    RAG: 2a.Rag agents

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from langchain_ollama import OllamaEmbeddings
+from langchain_ollama import OllamaEmbeddings, ChatOllama
+from langchain.agents import create_agent
 from langchain_core.vectorstores import InMemoryVectorStore
 import bs4
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.tools import tool
+from langchain.chat_models import init_chat_model
 
-embeddings = OllamaEmbeddings(model="deepseek-r1:1.5b")
+model = init_chat_model(
+        model_provider="ollama",
+        model="qwen2.5:7b",
+        temperature=0,
+        base_url="http://localhost:11434"
+        )
+
+embeddings = OllamaEmbeddings(model="qwen2.5:7b")
 vector_store = InMemoryVectorStore(embeddings)
 
 ## Loading Document
@@ -29,4 +39,33 @@ all_splits = text_splitter.split_documents(docs)
 ## Storing Document
 document_ids = vector_store.add_documents(documents=all_splits)
 
-print(document_ids[:3])
+# print(document_ids[:3])
+
+@tool(response_format="content_and_artifact")
+def retrieve_context(query: str):
+    """Retrieve information to help answer a query."""
+    retrieved_docs = vector_store.similarity_search(query, k=2)
+    serialized = "\n\n".join(
+        (f"Source: {doc.metadata}\nContent: {doc.page_content}")
+        for doc in retrieved_docs
+    )
+    return serialized, retrieved_docs
+
+
+tools = [retrieve_context]
+# If desired, specify custom instructions
+prompt = (
+    "You have access to a tool that retrieves context from a blog post. "
+    "Use the tool to help answer user queries."
+)
+agent = create_agent(model, tools, system_prompt=prompt)
+query = (
+    "What is the standard method for Task Decomposition?\n\n"
+    "Once you get the answer, look up common extensions of that method."
+)
+
+for event in agent.stream(
+    {"messages": [{"role": "user", "content": query}]},
+    stream_mode="values",
+):
+    event["messages"][-1].pretty_print()
